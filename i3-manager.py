63c71f5a5063cb419c6612a27d6b84d75732e755
George Lewis <georgejeffreylewis@gmail.com>
2019-08-03 19:05:31 -0400

    Implemented list-configs and list-parts

--- i3-manager.py
+++ i3-manager.py
@@ -1,6 +1,22 @@
-import argparse, hjson, configparser
+import argparse, hjson, configparser, os
 from pathlib import Path
 from os import sep
+
+def walk(directory, files_name):
+
+    print(f'{files_name.capitalize()} files:')
+
+    count = 0
+            
+    for root, _, files in os.walk(directory):
+        for file in files:
+            count += 1
+            print(f'{root}{sep}{file}')
+
+    if count == 0:
+        print(f"There's no {files_name} files")
+    else:
+        print(f"Found {count} {files_name} {'file' if count == 1 else 'files'}!")
 
 def main():
 
@@ -17,7 +33,7 @@ def main():
                     help='Use the specified config file')
 
     parser.add_argument('verb', type=str,
-                    help='The action to perform (list-configs, list-parts, generate)')
+                    help='The action to perform (list-configs, list-parts, list-all, generate, diff)')
 
     args = parser.parse_args()
 
@@ -30,15 +46,39 @@ def main():
 
     config = configparser.ConfigParser()
 
-    config_file = args.config if args.config else \
-                f'{Path.home()}{sep}.config{sep}i3-manager{sep}config.ini'
+    config_file = args.config if args.config else 'config.ini'
 
     if not config.read(config_file):
-        raise ValueError(f'Could not load config file: "{config_file}"') # Config couldn't be found
+        raise ValueError(f'Could not load config file: \"{config_file}\"') # Config couldn't be found
 
-    
+    if args.verbose >= 1:
+        print(f'Read config file \"{config_file}\"')
 
-    
+    #
+    # list-parts, list-configs, and list-all
+    #
+
+    if args.verb in ['list-parts', 'list-configs', 'list-all']:
+
+        if args.verb in ['list-parts', 'list-all']:
+
+            walk(config['Main']['parts_dir'], 'part')
+        
+        if args.verb in ['list-configs', 'list-all']:
+
+            walk(config['Main']['configs_dir'], 'config')
+
+    #
+    # Generate
+    #
+
+    elif args.verb == 'generate':
+        pass
+
+
+    else:
+        print(f'Unknown verb: \"{args.verb}\"')
+        print('Did nothing')
 
 if __name__ == "__main__":
     main()
